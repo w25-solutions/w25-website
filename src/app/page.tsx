4bab796cc17e57310af00eec9b5cf3597bd1e390
Wilson Neto <contato@wilsonneto.com.br>
2023-12-16 10:42:27 -0300

    adjust for azute static web app deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
               At our company, our mission is to revolutionize how individuals and businesses interact with technology.
             </p>
             <p>
-              We're dedicated to creating innovative products that automate and optimize tasks, enhancing efficiency and productivity. Our vision is to set the standard for automation and optimization, both personally and professionally.
+              We are dedicated to creating innovative products that automate and optimize tasks, enhancing efficiency and productivity. Our vision is to set the standard for automation and optimization, both personally and professionally.
             </p>
             <p>
               Our commitment to innovation, customer-centric design, and efficiency drives us to empower businesses and improve the quality of life. We value sustainability, adaptability, collaboration, integrity, and maintain a global perspective in all we do.
